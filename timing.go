4c762e10a74d3b7a114a32c32c28f328323ee668
Vink <dong@wenqi.us>
2017-11-20 17:06:13 +0800

    add request, sleep func

--- timing.go
+++ timing.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"github.com/VinkDong/asset-alarm/log"
 	"gopkg.in/yaml.v2"
+	"net/http"
+	"strings"
+	"time"
 )
 
 const CLR_0 = "\x1b[30;1m"
@@ -28,9 +31,9 @@ var (
 type Rule struct {
 	Method string
 	Url    string
-	Bodies map[string]string
+	Bodies map[string]string `yaml:"body"`
 	Range  map[string]map[string]int
-	Every  map[string]int
+	Every  map[string]int `yaml:"run_every"`
 }
 
 func main() {
@@ -40,6 +43,98 @@ func main() {
 	}
 	r := &Rule{}
 	parseYaml(r, *conf)
+	for {
+		if checkTimeIn(r) {
+			for body := range r.Bodies {
+				log.Infof("sending to %s ... ", r.Url)
+				sendRequest(r.Method, r.Url, r.Bodies[body])
+			}
+		}
+		d := getSleepTime(r)
+		log.Infof("sleepy for %s",d.String())
+		time.Sleep(d)
+	}
+}
+
+func getSleepTime(r *Rule) time.Duration {
+	every := r.Every
+	var duration time.Duration
+	if val, ok := every["seconds"]; ok {
+		duration = time.Duration(val) * time.Second
+	}
+	if val, ok := every["minutes"]; ok {
+		duration += time.Duration(val) * time.Minute
+	}
+	if val, ok := every["hours"]; ok {
+		duration += time.Duration(val) * time.Hour
+	}
+	if val, ok := every["day"]; ok {
+		duration += time.Duration(val) * time.Hour * 24
+	}
+	return duration
+}
+
+func checkTimeIn(r *Rule) bool{
+	current := time.Now().UTC()
+	if rH := r.Range["hour"]; rH != nil {
+		currentHour := current.Hour()
+		if !checkCondition(currentHour, rH){
+			log.Infof("hour %d not in request period",currentHour)
+			return false
+		}
+
+	}
+	return true
+}
+
+func checkCondition(current int, condition map[string]int) bool {
+	if v, ok := condition["gt"]; ok {
+		if current <= v{
+			return false
+		}
+	}
+
+	if v, ok := condition["gte"]; ok {
+		if current < v{
+			return false
+		}
+	}
+
+	if v, ok := condition["lt"]; ok {
+		if current >= v{
+			return false
+		}
+	}
+
+	if v, ok := condition["lte"]; ok {
+		if current > v{
+			return false
+		}
+	}
+
+	if v, ok := condition["eq"]; ok {
+		if current != v{
+			return false
+		}
+	}
+
+	return true
+}
+
+func sendRequest(method,url,body string){
+	client  := &http.Client{}
+	req, err := http.NewRequest(method,url,strings.NewReader(body))
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	resp, err := client.Do(req)
+	if err != nil{
+		log.Error(err)
+		return
+	}
+	data , err := ioutil.ReadAll(resp.Body)
+	log.Infof("%s",data)
 }
 
 func showHelp() {
