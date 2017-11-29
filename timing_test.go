f4cb57d1970287e69ac38e5aa748c66bac2526c6
Vink <dong@wenqi.us>
2017-11-29 16:24:49 +0800

    supplement readme and fix test case

--- timing_test.go
+++ timing_test.go
@@ -3,13 +3,14 @@ package main
 import "testing"
 
 func TestParseYaml(t *testing.T) {
-	r := &Rule{}
-	parseYaml(r, "./config.yaml")
+	rList := make([]Rule,0)
+	parseYaml(&rList,"./config.yaml")
+	r := rList[0]
 	if r.Method != "PUT"{
 		t.Errorf("test parse yaml file faild expect method is PUT but got %s",r.Method)
 	}
 
-	if r.Range["hour"]["gte"] != 7{
+	if r.Range["hour"]["gte"] != 0{
 		t.Errorf("test parse yaml file faild expect range hour is gte 7 but go %d",r.Range["hour"]["gte"])
 	}
 }
